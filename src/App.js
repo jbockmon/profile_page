54b05018041445f88a5d6037e849bde78f41f9aa
Jason Bockmon <jbockmon@gmail.com>
2019-08-24 09:31:57 -0500

    Created basic site framework with router.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Layout from './containers/Layout/Layout';
+import Experience from './containers/Experience/Experience';
+import Main from './containers/Main/Main';
+import Projects from './containers/Projects/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path={'/experience'} component={Experience}/>
+        <Route path={'/projects'} component={Projects}/>
+        <Route path={'/'} component={Main}/>
+      </Switch>
+    </Layout>
   );
 }
 
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+
+const Layout = (props) => {
+   return (
+      <>
+         <NavBar/>
+         {props.children}
+      </>
+   );
+   
+}
+
+export default Layout;
