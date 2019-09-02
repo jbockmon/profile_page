70a2b5a3c358e9035ba1c6c498a68a6512a71d13
Jason Bockmon <jbockmon@gmail.com>
2019-09-02 08:19:50 -0500

    Housekeeping on About section.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,8 +8,7 @@ const About = () => {
          <h2>About Me</h2>
          <p>
             Howdy! I am A full stack developer from Austin, TX with an emphasis in JavaScript, Node.js, and React development. 
-            I am a software engineer a long history of IT support and DevOps, with a strong professional background in developing and delivering solutions in both the hardware and software realms. 
-            I have administrated both Linux and Windows servers as well as supported desktop environments of all types. 
+            I am a software engineer with a long history of IT support and DevOps. I have a strong professional background in developing and delivering solutions in both the hardware and software realms. 
             <br/> <br/>
             I am a devoted father of two young children, and love to spend a day in the kitchen cooking, or an evening in the back yard by the grill. 
             I also make a mean brisket, but my wife would say my smoked turkey is far better. 
