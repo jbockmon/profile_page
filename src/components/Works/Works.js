6303060c1aaf2380424aae87852ae62f13c2d9b6
Jason Bockmon <jbockmon@gmail.com>
2020-07-11 10:39:39 -0700

    Edited Works formatting and content.

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -13,42 +13,52 @@ const Works = () => {
          'Nodejs back end',
          'Express based RESTful API',
          'React front end',
+         'Bootstrap',
          'React Timeseries Charts data visualization',
          'OpenVPN server',
          'Postgres database',
-         'Modbus/IP polling'
+         'Modbus/TCP polling'
       ], 
-      detail: 'New PLC architecture required a data aggregation system and service platform. The platform also had to allow for secure remote accessibility.',
+      detail: 
+         'This project was to replace the site level monitoring system of the previous generation control system. ' +
+         'It needed to provide a secure platform for servicing units remotely, as well as display critical sensor and diagnostic information ' +
+         'in a manner consistent with customer expectations. It also needed to compare aggregated data over time, as well as between devices' + 
+         'to accurately diagnose and predict problems within critical server infrastructure.',
       requirements: [
          'Able to integrate any number of PLCs',
          'DHCP server to provide local network to all connected PLCs',
          'OpenVPN server to allow access to both the DCIM platform as well as all local PLCs with Schneider proprietary software',
          'Update and alert data transmission resilience through lost internet connections',
-         'Advanced diagnostic capabilities that catch out of spec conditions as well as future conditions through trend analysis',
-         'Web API + Modbus + SNMP + Bacnet servers for integration with existing customer infrastructure',
+         'Advanced diagnostic capabilities that catch out of spec conditions as well as predict future conditions through trend analysis',
+         'REST API + Modbus + Bacnet servers for integration with existing customer infrastructure',
          'Ability to interface with multiple cloud DCIM providers including Schneider Machine Advisor'
       ]
    }
 
    let newPlc = {
-      title: 'PLC System Redesign',
+      title: 'Control System Redesign',
       summary: 'Creation and transition to new PLC based architecture.',
       role: 'Sole Architect, Engineer, Developer, QA, and DevOps',
       techSpecs: [
          'Structured Text based implementation',
-         'Ladder logic and FBD PID',
-         'Modbus RTU controlled pump drives',
-         'Modbus over IP register server'
+         'Ladder logic and Function Block Diagram PIDs',
+         'Modbus RTU controlled VFDs',
+         'Modbus TCP register server'
       ],
-      detail: 'Management decided to switch the flagship product from a custom x86 design with dated hardware and software to a modern PLC solution that monitors various sensors and adjusts controllers to cool servers in a liquid cooling environment.',
+      detail: 
+         'Management decided to switch the flagship product from a custom built x86 design with >10 year old hardware and software' +
+         'to a modern PLC solution that monitors various sensors and adjusts multiple controllers to cool servers in a liquid cooling environment.' +
+         'This product needed to have a unified code base that controls any combination of multiple VFDs, coolers, and valve actuators. It also had to support' +
+         'multiple products with differeing sensor and monitoring packages on the same code base. ',
       requirements: [
          '>99% uptime',
          '2N reliability',
-         'Runs 3 independent PIDs, controlling coolant flow, water flow, and secondary systems',
+         'Runs 3 independent PIDs, controlling coolant flow, water flow, and cooling tower speed',
          'Integration with Schneider VFDS',
-         'Self diagnosing and self correcting diagnostics',
+         'Self correcting diagnostics',
          'Able to run completely independently or able to integrate into a DCIM',
-         'Single codebase must run all products without custom configuration'
+         'Unified code base must run all products without custom builds',
+         'Web based interface'
       ]
    };
 
@@ -61,14 +71,20 @@ const Works = () => {
          'Express based RESTful API',
          'React front end',
          'React Timeseries Charts data visualization',
+         'Modbus Server for DCIM integration'
       ],
-      detail: 'This project was essentially a combination of a GUI for editing local configuration files in the brower and a monitoring system. It had to read various system sensors, analyze the data coming from them, and report whether or not they were within specs.',
+      detail: 
+         'This project was to modernize an aging product by providing a web interface for it. The web interface had to interact with various ' +
+         'system files and databases in order to display all pertinent information in a server cooling solution. It also had to act as a visual ' +
+         'interface for editing configuration files with automatic parsing, input scrubbing, and error detection to allow technicians to ' +
+         'utilize a browser instead of a linux terminal for system management.',
       requirements: [
          'Report sensor values in quickly ascertainable graphical format',
          'Utilize pre-existing CSV "database" to display rolling 7 day data in graph format',
          'Graphs must be user customizable',
          'Configuration GUI must be touch responsive in browser',
-         'Ability to execute already developed scripts from GUI'
+         'Ability to execute already developed scripts from GUI',
+         'Modbus server broadcast for Schneider EcoStruxure DCIM integration'
       ]
    };
 
